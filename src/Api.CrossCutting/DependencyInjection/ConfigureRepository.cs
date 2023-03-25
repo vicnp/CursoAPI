181e026e47b19d59da96c8921ebd8a123e0437eb
Victor Nascimento <vicnp10@gmail.com>
2023-03-24 21:10:07 -0300

    Injeção de Dependecia para Login

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +17,9 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceColletion)
         {
             serviceColletion.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceColletion.AddScoped<IUserRepository, UserImplementation>();
+
+
             serviceColletion.AddDbContext<MyContext>(
             options => options.UseMySql("Server=localhost;port=3306;Database=dbAPI;Uid=root;Pwd=123456")
         );
